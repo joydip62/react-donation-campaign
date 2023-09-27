2c1d1b03091749ba5ee42b6f1775c927bbeb7b88
JPS Archives <joydipbanik6@gmail.com>
2023-09-27 20:14:38 +0530

    added banner section

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,7 +2,28 @@
 
 const Banner = () => {
     return (
-      <div className="h-[70vh] bg-green-400">This is from home component</div>
+      <div>
+        <div className="h-[80vh] relative w-full">
+          <img
+            className="w-full h-full object-cover opacity-25"
+            src="./img/banner.png"
+            alt="Banner"
+          />
+
+          <div className="absolute w-full top-1/3">
+            <h1 className="text-5xl font-extrabold m-auto text-center pb-5">
+              I Grow By Helping People In Need
+            </h1>
+            <div className="text-center">
+              <input
+                type="text"
+                className="w-2/4 bg-white p-2 rounded-lg border-2	border-gray-300"
+              />
+              <button className="bg-red-600 text-white font-semibold px-4 py-2 rounded-lg">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 };
 
