53d3d30d177b0f1a8dc6ee4a9385cb1ccc5aecc2
JPS Archives <joydipbanik6@gmail.com>
2023-09-27 19:01:00 +0530

    show conditional data. if any data in local storage then show chart

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -5,40 +5,54 @@ import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
   const donations = useLoaderData();
-
-    const totalPrice = donations
-      .map((donation) => donation.price)
-      .reduce((acc, crrPrice) => acc + crrPrice, 0);
- 
-
   const [totalDonationPrice, setTotalDonationPrice] = useState(0);
+  const [noFound, setNoFound] = useState("");
+  
+  const totalPrice = donations
+    .map((donation) => donation.price)
+    .reduce((acc, crrPrice) => acc + crrPrice, 0);
 
   useEffect(() => {
     const donateItem = JSON.parse(localStorage.getItem("donations"));
-    const donationPrice = donateItem.reduce(
-      (preItem, currItm) => preItem + currItm.price,
-      0
-    );
-    setTotalDonationPrice(donationPrice);
+
+    if (donateItem) {
+      const donationPrice = donateItem.reduce(
+        (preItem, currItm) => preItem + currItm.price,
+        0
+      );
+      setTotalDonationPrice(donationPrice);
+    } else {
+      setNoFound(
+        <h2 className="h-[80vh] text-5xl font-extrabold flex justify-center items-center">
+          No Data Found
+        </h2>
+      );
+    }
   }, []);
 
-   const data = [
-     ["Donation", "Calculate donation"],
-     ["Total Donation", totalPrice],
-     ["Your Donation", totalDonationPrice],
-   ];
+  const data = [
+    ["Donation", "Calculate donation"],
+    ["Total Donation", totalPrice],
+    ["Your Donation", totalDonationPrice],
+  ];
 
   return (
     <div>
-      <h1 className="block m-auto w-full text-5xl font-semibold">
-        Your Total Donation {totalDonationPrice}
-      </h1>
-      <Chart
-        chartType="PieChart"
-        data={data}
-        width={"100%"}
-        height={"400px"}
-      />
+      {noFound ? (
+        noFound
+      ) : (
+        <div>
+          <h1 className="block m-auto w-full text-5xl font-semibold">
+            Your Total Donation {totalDonationPrice}
+          </h1>
+          <Chart
+            chartType="PieChart"
+            data={data}
+            width={"100%"}
+            height={"400px"}
+          />
+        </div>
+      )}
     </div>
   );
 };
